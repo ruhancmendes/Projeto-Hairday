2ee89d2f5d9897280e53308b03b23dba7251f61a
ruhancmendes <ruhancmendes@gmail.com>
2025-04-03 17:35:57 -0300

    webpack server install

---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+//definir as configurações do webpack
+const path = require("node:path");
+
+module.exports = {
+  target: "web",
+  mode: "development",
+
+  entry: path.resolve(__dirname, "src", "main.js"), // ponto de entrada do aplicativo
+  output: {
+    filename: "main.js", // nome do arquivo de saída
+    path: path.resolve(__dirname, "dist"), // diretório de saída
+  },
+
+  // Configurações do servidor de desenvolvimento
+  devServer: {
+    static: {
+      directory: path.join(__dirname, "dist"), // diretório onde os arquivos estáticos estão localizados
+    },
+    port: 3000,
+    open: true, // abre o navegador automaticamente
+    liveReload: true, // ativa o recarregamento automático
+  }
+};
