fe611ed7fbc7aa7ce473a639293c388202005464
ruhancmendes <ruhancmendes@gmail.com>
2025-04-09 20:27:54 -0300

    render appointments

--- src/modules/schedules/load.js
+++ src/modules/schedules/load.js
@@ -1,13 +1,21 @@
 // Carrega o agendamento do dia.
 
+import { scheduleFetchByDay } from "../../services/schedule-fetch-by-day.js"; //importa a função que busca os agendamentos do dia
 import { hoursLoad } from "../form/hours-load.js"; //importa a função que define os horários de funcionamento
+import { schedulesShow } from "../schedules/show.js"; //importa a função que renderiza os agendamentos
 
 //Seleciona o input de data.
 const selectedDate = document.getElementById("date");
 
-export function schedulesDay() {
+export async function schedulesDay() {
   // Obtém a data do input de data.
   const date = selectedDate.value;
+
+  // Busca na API os agendamentos 
+  const dailySchedules = await scheduleFetchByDay({ date})
+
+  // Exibe os agendamentos
+  schedulesShow({ dailySchedules });
 
   // Renderiza as horas disponíveis.
   hoursLoad({ date });
--- src/services/schedule-fetch-by-day.js
+++ src/services/schedule-fetch-by-day.js
@@ -7,19 +7,21 @@ import dayjs from "dayjs";
 import { apiConfig } from "./api-config.js";
 
 export async function scheduleFetchByDay({ date }) {
-    try {
-        // Busca os dados na API (fazendo a requisição.)
-        const response = await fetch(`${apiConfig.baseURL}/schedules`)
+  try {
+    // Busca os dados na API (fazendo a requisição.)
+    const response = await fetch(`${apiConfig.baseURL}/schedules`);
 
-        // Converte para JSON.
-        const data = await response.json()
-        
-        // Filtra os agendamentos pelo dia selecionado.
-        const dailySchedules = data.filter(( schedule) => 
-        dayjs(date).isSame(schedule.when, "day"))
-        
-    } catch (error) {
-        console.log(error);
-        alert("Não foi possível buscar os agendamentos do dia.");
-    }
+    // Converte para JSON.
+    const data = await response.json();
+
+    // Filtra os agendamentos pelo dia selecionado.
+    const dailySchedules = data.filter((schedule) =>
+      dayjs(date).isSame(schedule.when, "day")
+    );
+
+    return dailySchedules;
+  } catch (error) {
+    console.log(error);
+    alert("Não foi possível buscar os agendamentos do dia.");
+  }
 }
